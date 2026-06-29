f32b7ec12970f5c8f4cd70353566cf62d3fe0bbe
Pascal <pascal@proudinnovations.com>
2026-06-29 19:25:57 +0200

    Adjust admin hamburger breakpoint from min-width 1180px to 1181px

--- resources/views/layouts/app-admin.blade.php
+++ resources/views/layouts/app-admin.blade.php
@@ -12,7 +12,7 @@
     <style>
 [x-cloak]{display:none!important}
 @media (max-width: 1180px) { #desktop-sidebar { display: none !important; visibility: hidden !important; opacity: 0 !important; width: 0 !important; overflow: hidden !important; } }
-@media (min-width: 1180px) { #hamburger-btn { display: none !important; } }
+@media (min-width: 1181px) { #hamburger-btn { display: none !important; } }
 </style>
 </head>
 <body class="font-sans antialiased bg-gray-100">
